e343d19c6e41d0eeeea0146212fe60043befa28b
Jae <jeonjaeyeong@jeonjaeyeong-ui-MacBookAir.local>
2022-01-25 20:07:40 +0900

    firstsetting

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/23 00:28:38 by jaeyjeon          #+#    #+#             */
+/*   Updated: 2022/01/23 01:04:32 by jaeyjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
