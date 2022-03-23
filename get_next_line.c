17a94a6a093536527127142463be4e443618c070
Jae <jeonjaeyeong@jeonjaeyeong-ui-MacBookAir.local>
2022-03-23 15:22:58 +0900

    22.03.23/1522

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,103 @@
 /*   By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 00:27:53 by jaeyjeon          #+#    #+#             */
-/*   Updated: 2022/01/27 03:59:20 by jaeyjeon         ###   ########.fr       */
+/*   Updated: 2022/03/23 15:22:03 by jaeyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+char	*get_str(int fd, char *str)
+{
+	size_t	readsize;
+	char	*readbuf;
+
+	readbuf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (readbuf == NULL)
+		return (0);
+	readsize = 1;
+	while (readsize != 0 && !ft_strchr(str, '\n'))
+	{
+		readsize = read(fd, readbuf, BUFFER_SIZE);
+		if (readsize == -1)
+		{
+			free(readbuf);
+			return (0);
+		}
+		readbuf[readsize] = '\0';
+		str = ft_strjoin(str, readbuf);
+	}
+	free(readbuf);
+	return (str);
+}
+
+char	*give_line(char *backup)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	if (backup[i] == '\0')
+		return (0);
+	while (backup[i] != '\0' && backup[i] != '\n')
+		i++;
+	str = malloc(sizeof(char) * (i + 1));
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (backup[i] != '\n' && backup[i] != '\0')
+	{
+		str[i] = backup[i];
+		i++;
+	}
+	if (backup[i] == '\n')
+		str[i++] = '\n';
+	str[i] = '\0';
+	return (str);
+}
+
+char	*cut_line(char *backup)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	while (backup[i] != '\0' && backup[i] != '\n')
+		i++;
+	if (backup[i] == '\0')
+	{
+		free(backup);
+		return (0);
+	}
+	str = malloc(sizeof(char) * (sizeof(backup) - i + 1));
+	if (str == NULL)
+		return (0);
+	i++;
+	while (backup[i] != '\0')
+		str[j++] = backup[i++];
+	str[j] = '\0';
+	return (str);
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*backup[OPEN_MAX];
-	char		str[BUFFER_SIZE + 1];
-	int			readsize;
+	static char	*backup;
+	char		*str;
 
-	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE < 0)
-		return ("asd");
-	while (read(fd, str, BUFFER_SIZE))
+	if (BUFFER_SIZE <= 0 && fd < 0)
+		return (0);
+	get_str(fd, backup);
+	if (backup == NULL)
 	{
-		str[readsize] = '\0';
+		free(backup);
+		return (0);
 	}
-	return (NULL);
+	str = give_line(backup);
+	backup = cut_line(backup);
+	return (str);
 }
 
 #include <fcntl.h>
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 00:28:38 by jaeyjeon          #+#    #+#             */
-/*   Updated: 2022/01/27 01:55:51 by jaeyjeon         ###   ########.fr       */
+/*   Updated: 2022/03/23 15:04:02 by jaeyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,12 @@
 # include <limits.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-# endif
-
-# ifndef OPEN_MAX
-#  define OPEN_MAX 10240
 # endif
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
 
 #endif
